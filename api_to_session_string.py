2ffaf904dd3f6db021b707a9e9c317caad4fae88
Hobrus <bhobrus@gmail.com>
2023-10-11 20:31:38 +0300

    add devconfig example and requirements

--- api_to_session_string.py
+++ api_to_session_string.py
@@ -1,8 +1,8 @@
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-api_id = 111 # Замените на ваш API ID
-api_hash = '111'  # Замените на ваш API Hash
+api_id = 23049196 # Замените на ваш API ID
+api_hash = '1b7bb0d68f84898b4c2c008500ad4b21'  # Замените на ваш API Hash
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     print(client.session.save())
